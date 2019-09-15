3c81dbcb2d3c99ca7411d0528ba6fcc2a53e412e
AdamM <ajmrozinski@gmail.com>
2019-09-15 01:09:15 -0400

    Spotify actions fully complete

--- liri.js
+++ liri.js
@@ -8,16 +8,18 @@ const userInputArray = process.argv;
 const spotify = new SpotifyAPI(spotifyKey.spotify)
 switch (userInputArray[2]) {
     case "spotify-this-song":
+        const query = userInputArray[3] ? userInputArray[3]: "Ace of base the sign"
         spotify.search({
             type: "track",
-            query: userInputArray[3],
+            query: query,
             limit: 1
         }).then(response => {
-            console.log(response)
             const track = response.tracks.items[0];
-            console.log("Artist: "+ track.album.artists[0].name)
+            console.log(`Artist: ${track.album.artists[0].name}`);
+            console.log(`Song name is: ${track.name}`)
+            console.log(`Here is a link to a preview ${track.external_urls.spotify}`)
+            console.log(`Album name is: ${track.album.name}`)
         })
-
 
         break;
 
