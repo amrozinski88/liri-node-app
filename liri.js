c23f3fe54ce712a95fecdd7d6443426686d1008a
AdamM <ajmrozinski@gmail.com>
2019-09-15 02:07:11 -0400

    Completed OMDB query section

--- liri.js
+++ liri.js
@@ -2,13 +2,15 @@ const dotenv = require("dotenv").config();
 const axios = require("axios");
 const moment = require("moment");
 const SpotifyAPI = require("node-spotify-api");
-const spotifyKey = require("./keys.js");
+const keys = require("./keys.js");
 const userInputArray = process.argv;
+const fs = require("fs")
 
-const spotify = new SpotifyAPI(spotifyKey.spotify)
+
+const spotify = new SpotifyAPI(keys.spotify)
 switch (userInputArray[2]) {
     case "spotify-this-song":
-        const query = userInputArray[3] ? userInputArray[3]: "Ace of base the sign"
+        const query = userInputArray[3] ? userInputArray[3] : "Ace of base the sign"
         spotify.search({
             type: "track",
             query: query,
@@ -23,6 +25,34 @@ switch (userInputArray[2]) {
 
         break;
 
+    case "movie-this":
+        const movieQuery = userInputArray[3] ? userInputArray[3] : "Mr.Nobody."
+        const queryURL = `http://www.omdbapi.com/?apikey=${keys.omdb.key}&t=${movieQuery}&type=movie`
+        axios.get(queryURL).then(response => {
+            console.log(response.data)
+            const movie = response.data
+            console.log(`Movie Title: ${movie.Title}`);
+            console.log(`Year Released: ${movie.Year}`);
+            console.log(`IMDB Rating: ${movie.imdbRating}`);
+            console.log(`Rotten Tomatoes Rating: ${movie.Ratings[1].Value}`);
+            console.log(`Country Produced in: ${movie.Country}`);
+            console.log(`Language: ${movie.Language}`);
+            console.log(`Plot: ${movie.Plot}`);
+            console.log(`Cast: ${movie.Actors}`);
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+        break;
+    case "do-what-it-says":
+        fs.readFile("./random.txt","utf8",(err,data)=>{
+            if(err){
+                throw(err)
+            }
+            const contentArray = data.split(",")
+            console.log(contentArray)
 
+            
+            
+        })
 };
-
